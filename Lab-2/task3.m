92653ae4dd1e304c39506e9ea067e0b6d7b1de0e
AndrzejSwietek2001 <mr.andi321@gmail.com>
2023-05-15 11:05:45 +0200

    Added Lab 2

---
+++ Lab-2/task3.m
@@ -0,0 +1,12 @@
+clc;clear;
+
+% Oblicz 3 potge liczby od urzytkownika
+x = input("Podaj liczbe: ");
+y = x.^3;
+disp(y)
+
+% wykonaj mnozenie macierzy [1 3 ; 5 9] i [4 -7 ; 10 0]
+A = [1 3 ; 5 9];
+B = [ 4 -7 ; 10 0 ];
+
+disp(A*B)
